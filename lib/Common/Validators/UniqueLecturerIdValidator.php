4979823cc63c874c078d9a5b4445400502750bc1
chuchudz999 <chuchudz999@gmail.com>
2024-07-25 16:19:48 +0700

    feat: Add a method check the existence of IDs and Emails of lecturers

---
+++ lib/Common/Validators/UniqueLecturerIdValidator.php
@@ -0,0 +1,29 @@
+<?php
+
+class UniqueLecturerIdValidator extends ValidatorBase implements IValidator
+{
+    private $_lecturerid;
+    private $_userid;
+    private $userRepository;
+
+    public function __construct(IUserViewRepository $userRepository, $lecturerid, $userid = null)
+    {
+        $this->_lecturerid = $lecturerid;
+        $this->_userid = $userid;
+        $this->userRepository = $userRepository;
+    }
+
+    public function Validate()
+    {
+        $this->isValid = true;
+        $userId = $this->userRepository->LecturerExists(null, $this->_lecturerid);
+
+        if (!empty($userId)) {
+            $this->isValid = $userId == $this->_userid;
+        }
+
+        if (!$this->isValid) {
+            $this->AddMessageKey('UniqueLecturerIdRequired');
+        }
+    }
+}
